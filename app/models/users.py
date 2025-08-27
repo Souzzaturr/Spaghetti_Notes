b1fdc299da3489f8087fd87ea506b6ad21d8bb7e
Arttur Souzza <souzzaaturr@gmail.com>
2025-08-27 11:06:44 -0300

    Adiciona login e cadastro de usuario

--- app/models/users.py
+++ app/models/users.py
@@ -19,7 +19,7 @@ def criar_usuario (nome: str, senha: str) -> None:
 
     hash_senha = security.dar_hash(senha)
 
-    with open("../../data/users_data.csv", "a", encoding = "UTF-8") as users_data:
+    with open("data/users_data.csv", "a", encoding = "UTF-8") as users_data:
         users_data.write(f"{nome},{hash_senha}\n")
     
     return
@@ -32,7 +32,7 @@ def nome_usuario_caracteres_permitidos (usuario: str) -> bool:
     for caractere in usuario:
 
         #verificar se o caractere não é letra, numero ou underline
-        if ord(caractere) < 48 or ord(caractere) > 57 or ord(caractere) < 65 or ord(caractere) > 90 or ord(caractere) < 95 or ord(caractere) > 95 or ord(caractere) < 97 or ord(caractere) > 122:
+        if not (caractere.isalnum() or caractere == "_"):
 
             return False
     
