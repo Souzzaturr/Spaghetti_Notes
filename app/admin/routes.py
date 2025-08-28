d1a4c4c80fc16005a0d82e1eb33c79034b6f77d4
Arttur Souzza <souzzaaturr@gmail.com>
2025-08-28 09:26:01 -0300

    Adiciona rota para /admin

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,4 +1,18 @@
-from flask import Blueprint, request, redirect, url_for, render_template, current_app
+from flask import Blueprint, request, redirect, url_for, render_template, current_app, session, flash
+from ..models import posts
 from . import admin_bp
 
-#@admin_bp.route("/")
+@admin_bp.route("/admin")
+def admin ():
+
+    if "usuario" in session.keys():
+        if session["usuario"] == "Artur_ADM":
+
+            flash("Seja bem vindo... ADM SUPREMO!!", "success")
+
+            return render_template("admin.html")
+
+
+    flash("Você não tem acesso à essa pagina!!", "danger")
+
+    return redirect(url_for("main.inicial"))
