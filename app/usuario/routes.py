0496aab2fa923737a187c19f6498a1e4d87c8066
Arttur Souzza <souzzaaturr@gmail.com>
2025-08-27 22:51:23 -0300

    Adiciona funionalidade de postar receita + funcionalidade de usuario realizar logout + perfil do usuario

--- app/usuario/routes.py
+++ app/usuario/routes.py
@@ -1,3 +1,15 @@
+from flask import render_template, request, session, flash, redirect, url_for
 from . import usuario_bp
+from ..models import posts
 
-#@usuario_bp.route("/perfil")
+@usuario_bp.route("/perfil")
+def perfil ():
+
+    if not "usuario" in session.keys():
+        flash("Para acessar essa rota é preciso estar logado!!", "danger")
+
+        return redirect(url_for("usuario.login"))
+
+    lista_posts = posts.exibir_posts_usuario(session["usuario"])
+
+    return render_template("perfil.html", lista_posts = lista_posts, usuario = session["usuario"])
