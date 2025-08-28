174d66b04c8a2303426e9b690812c3c0bafad80e
Artur da Silva Souza <202514320010@ad.ifpb.edu.br>
2025-08-28 18:54:22 -0300

    Corrige bug de quebra de linhas

--- app/models/data_treatment.py
+++ app/models/data_treatment.py
@@ -4,14 +4,14 @@
 
 def tratar_texto_post (texto: str) -> str:
     texto_tratado = texto.replace("\r\n", "\n")
-    texto_tratado = texto_tratado.replace(",", ";;").replace("\n", "/////")
+    texto_tratado = texto_tratado.replace(",", ";;").replace("\n", "bR3@klIn3z")
 
     return texto_tratado
 
 #---
 
 def reverter_tratamento_texto_post (texto_tratado: str) -> str:
-    texto = texto_tratado.replace("/////", "\n")
+    texto = texto_tratado
     texto = texto.replace(";;", ",")
 
     return texto
