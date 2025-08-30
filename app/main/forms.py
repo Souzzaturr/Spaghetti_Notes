31061777812dc943205a26ee895f030dc10ee3e5
Arttur Souzza <souzzaaturr@gmail.com>
2025-08-30 15:44:10 -0300

    Adiciona tratamento de espaços extras em titulo e conteudo receita

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, url_for, render_template, session, flash, redirect
 from . import main_bp
-from ..models import posts
+from ..models import posts, data_treatment
 
 
 
@@ -12,18 +12,18 @@ def postar_receita ():
         if "usuario" in session.keys():
 
             titulo = request.form.get("titulo")
-
             conteudo = request.form.get("receita")
+
+            titulo = data_treatment.remover_espacos(titulo)
+            conteudo = data_treatment.remover_espacos(conteudo)
 
             posts.criar_post(titulo, conteudo, session["usuario"])
 
             flash("Post de receita criado com sucesso!!", "success")
-
             return redirect(url_for("main.inicial"))
 
         
         flash("Você precisa estar logado para criar um post de receita!!", "danger")
-
         return redirect(url_for("main.postar_receita"))
     
     
